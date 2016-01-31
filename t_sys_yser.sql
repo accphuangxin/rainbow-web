03c29bacff9f4185f9c869511443355746b89efa
accphuangxin <xin.huang@LT-279.howbuy.domain>
2016-02-01 00:26:43 +0800

    添加使用说明，修改jetty配置。增加示例SQL脚本

---
+++ t_sys_yser.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.24-log : Database - db1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db1`;
+
+/*Table structure for table `t_sys_user` */
+
+DROP TABLE IF EXISTS `t_sys_user`;
+
+CREATE TABLE `t_sys_user` (
+  `guid` varchar(32) NOT NULL,
+  `user_no` decimal(10,0) DEFAULT NULL,
+  `user_name` varchar(20) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `creater` varchar(20) DEFAULT NULL,
+  `modifyer` varchar(20) DEFAULT NULL,
+  `modify_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
